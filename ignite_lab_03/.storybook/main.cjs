e5c9c844f311424d49f6638535b79986375166a3
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-10-16 12:48:45 -0300

    ci: Add publish storybook workflow

--- ignite_lab_03/.storybook/main.cjs
+++ ignite_lab_03/.storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if(configType === 'PRODUCTION') {
-      config.base = '/ignite_lab/ignite_lab_03/'
+      config.base = '/ignite_lab/'
     }
   }
 }
