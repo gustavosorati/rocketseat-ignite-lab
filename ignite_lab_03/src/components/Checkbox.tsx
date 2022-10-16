1dd03227cb2853d2cb7be68aa4fe5b13be5a39a3
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-10-16 19:33:19 -0300

    Aula 03

--- ignite_lab_03/src/components/Checkbox.tsx
+++ ignite_lab_03/src/components/Checkbox.tsx
@@ -1,13 +1,12 @@
 import * as CheckBoxPrimitive from '@radix-ui/react-checkbox';
-import { clsx } from 'clsx'
 import { Check } from 'phosphor-react';
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckBoxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
 
   return (
-    <CheckBoxPrimitive.Root className={"w-6 h-6 p-[2px] bg-gray-800 rounded"} >
+    <CheckBoxPrimitive.Root className={"w-6 h-6 p-[2px] bg-gray-800 rounded"} {...props} >
       <CheckBoxPrimitive.CheckboxIndicator asChild>
         <Check size={20} weight="bold" className='h-5 w-5 text-cyan-500' />
       </CheckBoxPrimitive.CheckboxIndicator>
