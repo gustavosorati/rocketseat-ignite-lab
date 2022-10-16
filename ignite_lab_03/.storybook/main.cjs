9a93b6b2edd03d7054eb9c84352299a651730012
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-10-16 13:17:16 -0300

    ci: Try to run a package on subpaste

--- ignite_lab_03/.storybook/main.cjs
+++ ignite_lab_03/.storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if(configType === 'PRODUCTION') {
-      config.base = '/ignite_lab/'
+      config.base = '/ignite_lab_03/'
     }
   }
 }
